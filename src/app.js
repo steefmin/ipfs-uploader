a75961c5a3f06221d3d7070f004c1f639d2c726a
Steef Min <steefmin@gmail.com>
2017-11-24 15:52:31 +0100

    missing some  curly stuff

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const node = new Ipfs({ repo: 'ipfs-' + Math.random() })
 var xhttp = new XMLHttpRequest()
 
 node.once('ready', () => {
-  console.log('Online status: ', node.isOnline() ? 'online' : 'offline')
 
+  console.log('Online status: ', node.isOnline() ? 'online' : 'offline')
   document.getElementById("status").innerHTML= 'Node status: ' + (node.isOnline() ? 'online' : 'offline')
 
   node.files.add(new node.types.Buffer('Hello world!'), (err, filesAdded) => {
+
     if (err) {
       return console.error('Error - ipfs files add', err, res)
     }
 
-    filesAdded.forEach(
-      (file) => console.log('successfully stored', file.hash)
+    filesAdded.forEach((file) => {
+
+      console.log('successfully stored', file.hash)
+
       node.id(function (err, identity)  {
         if (err) {
           console.log(err)
@@ -21,6 +24,9 @@ node.once('ready', () => {
         xhttp.setRequestHeader("Content-Type", "application/json")
         xhttp.send(JSON.stringify({clientAddress:identity.addresses[0], ipfsPath:'QmQzCQn4puG4qu8PVysxZmscmQ5vT1ZXpqo7f58Uh9QfyY'}))
       })
-    )
+
+    })
+
   })
+
 })
