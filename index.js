9633f034f8399904a446619b5ce056a5128001e3
Steef Min <steefmin@gmail.com>
2017-11-24 14:59:38 +0100

    first try

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+let express = require('express')
+let logger = require('morgan');
+let path = require('path');
+let app = express();
+let Ipfs = require('ipfs')
+
+let node = new Ipfs()
+
+app.use(logger('dev'))
+
+app.use(express.static(path.join(__dirname, 'src')))
+
+let port = process.env.port || 5000
+
+app.listen(port)
+console.log('listening on port: ' + port)
