43fb21e715718c48970a2290394dcde69daa0273
Steef Min <steefmin@gmail.com>
2017-11-24 15:09:34 +0100

    process.env.PORT capitalize

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(logger('dev'))
 
 app.use(express.static(path.join(__dirname, 'src')))
 
-let port = process.env.port || 5000
+let port = process.env.PORT || 5000
 
 app.listen(port)
 console.log('listening on port: ' + port)
