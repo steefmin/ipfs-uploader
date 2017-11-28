ffd94f5908eed79992a4d736c74accfdc9370728
Steef Min <steefmin@gmail.com>
2017-11-28 13:46:55 +0100

    try ws

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ let node = new IPFS({
       Swarm: [
         '/ip4/0.0.0.0/tcp/4002',
         '/ip4/0.0.0.0/tcp/4003/ws',
-        '/dns4/ws-star.discovery.libp2p.io/wss/p2p-websocket-star'
+        '/dns4/ws-star.discovery.libp2p.io/wss/p2p-websocket-star',
+        '/dns4/ws-star.discovery.libp2p.io/ws/p2p-websocket-star'
       ]
     }
   }
